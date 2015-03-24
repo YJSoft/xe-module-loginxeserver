9686de9d1c441b3a6426143c01495b0db96e374a
YJSoft <yjsoft@yjsoft.pe.kr>
2015-03-24 18:29:13 +0900

    #1 SEC-001
    권한이 없는 도메인에서 로그인XE 서버 모듈을 사용할 수 있는 문제 수정

--- loginxeserver.admin.controller.php
+++ loginxeserver.admin.controller.php
@@ -1,29 +1,34 @@
 <?php
 class loginxeserverAdminController extends loginxeserver
 {
-  function init()
-  {
-  }
-  function procLoginxeserverAdminInsertConfig()
-  {
-    $oModuleController = getController('module');
+	function init()
+	{
+	}
+	function procLoginxeserverAdminInsertConfig()
+	{
+		$oModuleController = getController('module');
 
-    $config = Context::getRequestVars();
+		$config = Context::getRequestVars();
 
-    if(substr($config->def_url,-1)!='/')
-    {
-      $config->def_url .= '/';
-    }
+		if(substr($config->def_url,-1)!='/')
+		{
+			$config->def_url .= '/';
+		}
 
-    if(substr($config->githubdef_url,-1)=='/')
-    {
-      $config->githubdef_url = substr($config->githubdef_url,0,strlen($config->githubdef_url)-1);
-    }
+		if(substr($config->githubdef_url,-1)=='/')
+		{
+			$config->githubdef_url = substr($config->githubdef_url,0,strlen($config->githubdef_url)-1);
+		}
 
-    $oModuleController->updateModuleConfig('loginxeserver', $config);
+		//remove whitespace
+		$replaceStr = array("\r", " ", "\t", "\xC2\xAD");
+		$config->loginxe_domains = str_replace($replaceStr, '', $config->loginxe_domains);
+		$config->loginxe_domains = explode("\n",$config->loginxe_domains);
+
+		$oModuleController->updateModuleConfig('loginxeserver', $config);
 
 
-    $this->setMessage('success_updated');
-    $this->setRedirectUrl(getNotEncodedUrl('', 'module', 'admin', 'act', 'dispLoginxeserverAdminConfig'));
-  }
+		$this->setMessage('success_updated');
+		$this->setRedirectUrl(getNotEncodedUrl('', 'module', 'admin', 'act', 'dispLoginxeserverAdminConfig'));
+	}
 }
